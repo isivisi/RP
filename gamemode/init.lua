815e14f245d3b07aae71848bbc0fa326745a49e8
EnvoyConvoy <trundle2369@gmail.com>
2014-06-25 13:35:44 -0500

    Updated to accommodate teams
    Don't know how to restrict team 2 aka cops, or how to let allowed cops to switch

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -18,17 +18,18 @@ function GM:PlayerInitialSpawn( pl ) // this is when the player first joins
 	pl:SetGravity( 1 ) // Default gravity
 	pl:SetWalkSpeed( 250 ) // Default 
 	pl:SetRunSpeed( 500 ) // Default
+	pl:SetTeam( 1 ) // this will set the player to team 1 by default
 end
 
-
-function GM:PlayerLoadout( pl ) // should we commit this to the above function as it is the initial spawn
-
-	pl:GiveAmmo( 255,	"Pistol", 		true ) //I don't think this is necessary if they start witn no weapon
-	
-	pl:Give( "empty_weapon" ) // should we make it "weapon_crowbar" ?
 end
 
 function GM:PlayerSpawn( pl ) //This is every time after; so if they die and respawn this will occur
-		
-	pl:Give( "empty_weapon" ) // Default based on loadout
+	if pl:Team() == 1 then
+		pl:Give( "empty_weapon" ) // Default based on loadout
+	end
+	if pl:Team() == 2 then
+		pl:Give( "weapon_stunstick" )
+		pl:Give( "weapon_pistol" )
+		pl:Give( "weapon_357" )
+	end
 end
