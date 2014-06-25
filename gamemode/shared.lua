cdc6ae38f406b767e1238e03b25ea9f57a58ed03
EnvoyConvoy <trundle2369@gmail.com>
2014-06-25 13:53:47 -0500

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -15,4 +15,7 @@ team.SetUp(2, "Police", Color(51, 51, 255)) // Police Team
 
 
 util.PrecacheMode1("models/player/Group01/Male_01.mdl")
+util.PrecacheMode1("models/player/Group01/Male_02.mdl")
+util.PrecacheMode1("models/player/Group01/Male_03.mdl")
+util.PrecacheMode1("models/player/Group01/Male_04.mdl")
 util.PrecacheMode1("models/player/Police.mdl")
