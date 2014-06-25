a6acb81dd83e1605170850fdee23e2ca413cc6a1
EnvoyConvoy <trundle2369@gmail.com>
2014-06-25 13:00:29 -0500

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -14,10 +14,25 @@ include( 'shared.lua' )
    Name: gamemode:PlayerLoadout( )
    Desc: Give the player the default spawning weapons/ammo
 ---------------------------------------------------------*/
-function GM:PlayerLoadout( pl )
+function GM:PlayerInitialSpawn( pl ) -- this is when the player first joins
+	pl:SetGravity( 1 ) -- Default gravity
+	pl:SetWalkSpeed( 250 ) -- Default 
+	pl:SetRunSpeed( 500 ) --I don't think it needs to be this high for RP, but this is the default RunSped
 
-	pl:GiveAmmo( 255,	"Pistol", 		true )
+end
+
+
+function GM:PlayerLoadout( pl ) -- should we commit this to the above function as it is the initial spawn
+
+	pl:GiveAmmo( 255,	"Pistol", 		true ) --I don't think this is necessary if they start witn no weapon
 	
-	pl:Give( "empty_weapon" )
+	pl:Give( "empty_weapon" ) -- should we make it "weapon_crowbar" ?
+	
+end
+
+function GM:PlayerSpawn( pl ) --This is every time after; so if they die and respawn this will occur
+		
+	pl:Give( "empty_weapon" ) -- Default based on loadout
+	
 	
 end
