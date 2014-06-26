552b15dbc8a97ea40b1a3acd58cf931c96d1fa8e
isivisi <johniannandrea@gmail.com>
2014-06-26 16:59:16 -0400

    added data class

---
+++ gamemode/Data.lua
@@ -0,0 +1,11 @@
+// data class
+// used to simplifly data editing
+
+Data = class(function(d, loc)
+	d.loc = loc // this would be the location of the data, ex "c:/gmod/garrysmod/gamemodes/RP/data
+	end)
+	
+function Data:setUserMoney(userid, amount)
+	// to do
+
+end
