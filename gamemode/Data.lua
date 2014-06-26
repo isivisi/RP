51432bc31f75a23ee1786e2fc0bb3f2a50e8f828
isivisi <johniannandrea@gmail.com>
2014-06-26 17:35:42 -0400

    added idea for how data will be stored

--- gamemode/Data.lua
+++ gamemode/Data.lua
@@ -1,6 +1,21 @@
 // data class
 // used to simplifly data editing
 
+/* data stucture:
+	each user has 2 files, info and items. info contains money, job and other simple things and items contains all the items they have.
+	
+	for each player:
+		-- info --
+		money\n
+		job\n
+		
+		-- items -- 
+		itemname\n
+		itemname\n 
+		etc...
+	
+*/
+
 Data = class(function(d, loc)
 	d.loc = loc // this would be the location of the data, ex "c:/gmod/garrysmod/gamemodes/RP/data
 	end)
