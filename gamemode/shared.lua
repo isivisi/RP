05d2b968f599bc9f29ebac742c90c67b3c56c6a8
EnvoyConvoy <trundle2369@gmail.com>
2014-06-26 15:56:17 -0500

    Updated precache and the civmdl array
    added more for better variety, as I noticed I cycled mostly through only two when testing, so this will hopefully keep it from being repetitive. also changed the GM stuff at the top

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,8 +1,6 @@
-
-GM.Name 	= "Skeleton Gamemode"
-GM.Author 	= "N/A"
-GM.Email 	= "N/A"
-GM.Website 	= "N/A"
+GM.Name 	= "skeleton-gamemode"
+GM.Author 	= "Isi and Schooled"
+GM.Website 	= "www.impulse-gamers.com/forums"
 
 function GM:Initialize()
 
@@ -13,7 +11,13 @@ end
 CivMdl = { "models/player/Group01/Male_01.mdl",
 "models/player/Group01/Male_02.mdl",
 "models/player/Group01/Male_03.mdl",
-"models/player/Group01/Male_04.mdl" }
+"models/player/Group01/Male_04.mdl",
+"models/player/Group01/Male_05.mdl",
+"models/player/Group01/Male_06.mdl",
+"models/player/Group01/Male_07.mdl",
+"models/player/Group01/Male_08.mdl",
+"models/player/Group01/Male_09.mdl"
+ }
 
 team.SetUp(1, "Civilian", Color(255, 0, 0)) // Civilian Team
 team.SetUp(2, "Police", Color(51, 51, 255)) // Police Team
@@ -23,4 +27,9 @@ util.PrecacheModel("models/player/Group01/Male_01.mdl")
 util.PrecacheModel("models/player/Group01/Male_02.mdl")
 util.PrecacheModel("models/player/Group01/Male_03.mdl")
 util.PrecacheModel("models/player/Group01/Male_04.mdl")
+util.PrecacheModel("models/player/Group01/Male_05.mdl")
+util.PrecacheModel("models/player/Group01/Male_06.mdl")
+util.PrecacheModel("models/player/Group01/Male_07.mdl")
+util.PrecacheModel("models/player/Group01/Male_08.mdl")
+util.PrecacheModel("models/player/Group01/Male_09.mdl")
 util.PrecacheModel("models/player/Police.mdl") 
